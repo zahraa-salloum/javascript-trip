e203579d053f78e701f9d492e0379563e25a7ae4
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 01:58:51 +0200

    Implemented the sort function

--- javascript/mergesort.js
+++ javascript/mergesort.js
@@ -1,3 +1,28 @@
 var arr = [];
 for(var i = 0; i < 10; i++)
    arr.push(prompt("Enter a number"));
+
+function merge(left, right) {
+    var arr = []
+    while (left.length && right.length) {
+       if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    return [ ...arr, ...left, ...right ]
+}
+
+function mergeSort(array) {
+    var half = array.length / 2
+    
+    if(array.length < 2){
+      return array 
+    }
+    
+    var left = array.splice(0, half)
+    return merge(mergeSort(left),mergeSort(array))
+  }
+
+  var sorted_arr = mergeSort(arr);
