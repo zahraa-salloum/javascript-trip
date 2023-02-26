6d0264864b039781b9d9d58d11f7aa34d96011b3
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 14:23:33 +0200

    Added lets check button

--- javascript/isPalindrome.js
+++ javascript/isPalindrome.js
@@ -1,3 +1,8 @@
 function toMain(){
     window.location.href='main.html';
  }
+function displayMessage() {
+    var input_user = document.getElementById("inputIsPalindrome").value;
+    // document.getElementById("messageDisplay").innerHTML = input_user;
+}
+ 
