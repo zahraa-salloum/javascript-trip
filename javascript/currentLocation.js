a3a80f352038abd9b65e9569672e1ec60449fbc5
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 18:18:11 +0200

    Implemented position function and displayed it

--- javascript/currentLocation.js
+++ javascript/currentLocation.js
@@ -1,3 +1,13 @@
 function toMain(){
     window.location.href='main.html';
  }
+ var pos = document.getElementById("messageDisplay");
+ function getLocation() {
+   if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(showPosition);
+   }
+ }
+ function showPosition(position) {
+    pos.innerHTML = "Latitude: " + position.coords.latitude + 
+     "<br>Longitude: " + position.coords.longitude;
+   }
