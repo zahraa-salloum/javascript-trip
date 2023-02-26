65e7bc903394d088fcc4bbe9352f213b65a60334
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 21:37:54 +0200

    Implemented validate email

--- javascript/form.js
+++ javascript/form.js
@@ -12,19 +12,33 @@ var password = document.getElementById("password")
 var submit = document.getElementById("submit")
 var jsonText = document.getElementById("jsontext")
 
+
+function validateEmail() 
+{
+    var mail = document.getElementById('email').value;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+    {
+        return (true)
+    }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
+
+
 function validatePassword() {
-    var p = document.getElementById('password').value,
+    var p = document.getElementById('password').value;
         errors = [];
     if (p.length < 8) {
         errors.push("Your password must be at least 8 characters"); 
     }
-    if (p.search(/[a-z]/i) < 0) {
-        errors.push("Your password must contain at least one letter.");
+    if (p.search(/[A-Z]/) < 0) {
+        errors.push("Your password must contain at least one capital letter.");
     }
     if (p.search(/[0-9]/) < 0) {
         errors.push("Your password must contain at least one digit."); 
     }
-    if(p.search(/^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/) < 0){
+    if(p.search(/[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]/) < 0){
         errors.push("Your password must contain at least one special character."); 
     }
     if (errors.length > 0) {
@@ -44,7 +58,8 @@ submit.addEventListener("click", function(){
         "email":email.value,
         "password":password.value
     }
-    if(validatePassword()){
+    
+    if(validateEmail() && validatePassword()){
     jsonText.innerHTML = JSON.stringify(data)
     }
 })
