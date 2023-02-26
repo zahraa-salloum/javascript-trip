70aa857c3a884dbe3d088e434154bad17d7e8768
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 20:56:26 +0200

    Implemented JSON object

--- javascript/form.js
+++ javascript/form.js
@@ -1,3 +1,26 @@
 function toMain(){
     window.location.href='main.html';
  }
+
+
+var firstname = document.getElementById("firstname")
+var lastname = document.getElementById("lastname")
+var username = document.getElementById("username")
+var email = document.getElementById("email")
+var password = document.getElementById("password")
+
+var submit = document.getElementById("submit")
+var jsonText = document.getElementById("jsontext")
+
+
+
+submit.addEventListener("click", function(){
+    var data = {
+        "firstName":firstname.value,
+        "lastName":lastname.value,
+        "username":username.value,
+        "email":email.value,
+        "password":password.value
+    }
+    jsonText.innerHTML = JSON.stringify(data)
+})
