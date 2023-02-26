6355cbddf61151bd5a521b39b25493b3fd0e1208
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 15:15:03 +0200

    Implemented calculate age from year function

--- javascript/testAge.js
+++ javascript/testAge.js
@@ -1,3 +1,11 @@
 function toMain(){
     window.location.href='main.html';
  }
+function displayMessage() {
+    var input_user = Number(document.getElementById("inputYear").value);
+}
+
+function calculateAge(year){
+    var current_year = new Date().getFullYear()
+    return age = current_year - year;
+}
