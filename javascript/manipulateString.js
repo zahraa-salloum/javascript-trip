e26421792bf30e1576f70a0c62079264212396d4
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 16:09:54 +0200

    Implemented the manipulate string function

--- javascript/manipulateString.js
+++ javascript/manipulateString.js
@@ -1,3 +1,23 @@
 function toMain(){
     window.location.href='main.html';
  }
+
+
+ function manipulateString(str) {
+    
+    var changed_string = "";
+    var vowels = /[aeiou]/gi;
+  
+    
+    if (str[0].match(vowels)) {
+        changed_string = str + "ay";
+    } else if (str.match(vowels) === null) {
+        changed_string = str + "ay";
+    } else {
+        var vowel_index = str.indexOf(str.match(vowels)[0]);
+        changed_string = str.substr(vowel_index) + str.substr(0, vowel_index) + "ay";
+    }
+    return changed_string;
+  }
+
+  
