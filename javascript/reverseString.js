deed426efc083bcaad88b1ca3d910a3f2068fd85
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 19:46:56 +0200

    Implemented reverse only numbers instead of all sring

--- javascript/reverseString.js
+++ javascript/reverseString.js
@@ -4,14 +4,29 @@ function toMain(){
 
  function displayMessage() {
     var input_user = document.getElementById("inputString").value;
-    document.getElementById("messageDisplay").innerHTML = "The reversed string is: "+reverseString(input_user);
+    document.getElementById("messageDisplay").innerHTML = "The reversed string is: "+reverseOnlyNumbers(input_user);
  }
 
-function reverseString(str) {
-
-    var newString = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
+ var reverseOnlyNumbers = function(str) {
+    str = str.split('');
+    var start = 0;
+    var end = str.length - 1;
+    var regex = /[0-9]/g; 
+    while (start < end) {
+        if (str[start].match(regex) === null) { 
+            start++; 
+            continue;
+        }
+        if (str[end].match(regex) === null) { 
+            end--; 
+            continue;
+        }
+        var tmp = str[start]; 
+        str[start] = str[end];
+        str[end] = tmp;
+        start++;
+        end--;
     }
-    return newString;
-}
+    
+    return str.join('');
+};
