4f5bcb2e3c29b7f457b927252ba8684e6f17311a
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 14:34:20 +0200

    Implemented isPalindrome function

--- javascript/isPalindrome.js
+++ javascript/isPalindrome.js
@@ -5,4 +5,17 @@ function displayMessage() {
     var input_user = document.getElementById("inputIsPalindrome").value;
     // document.getElementById("messageDisplay").innerHTML = input_user;
 }
- 
+function checkIfPalindrome( str )
+{
+    for(var i = 0 ; i < (str.length -1)/2 ;i++)
+  {
+        var x = str[i] ;
+        var y = str[str.length -1-i];
+        if( x != y)
+        {
+            return false;
+        }
+  }
+        return true;
+}
+
