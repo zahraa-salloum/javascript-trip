d0f50ea007f291b7e1452f96fa3ffcdad087cf32
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-26 22:02:43 +0200

    Implemented the class

--- javascript/course.js
+++ javascript/course.js
@@ -1,3 +1,26 @@
 function toMain(){
     window.location.href='main.html';
  }
+ var courseName = document.getElementById("courseName");
+ var university = document.getElementById("university");
+ var year = document.getElementById("year");
+ var instructor = document.getElementById("instructor");
+
+ var submit = document.getElementById("submit")
+
+ class Course {
+    constructor(name, university, year, instructor) {
+      this.name = name;
+      this.university = university;
+      this.year = year;
+      this.instructor = instructor;
+    }
+ }
+var course;
+ submit.addEventListener("click", function(){
+
+    course = new Course(courseName.value, university.value,year.value,instructor.value);
+
+
+ })
+
